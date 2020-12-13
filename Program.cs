002869c7ff7f9d47154a83745cbf2c40d802e897
Allangr-eab <allangabrielrvsantos@yahoo.com.br>
2020-12-13 19:59:32 -0300

    Altera a página inicial do repositório

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NumeralExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console . Clear ();
+            Console . WriteLine ( " --- Extenso Numeral " );
+            Console . Write ( " Digite qualquer número: " );
+            int  numeroDigitado  =  Convert . ToInt32 ( Console . ReadLine ());
+            string  numeroExtenso  =  numeroDigitado . ToString ();
+            Console . WriteLine (" Número por extenso: { numeroExtenso } ");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # NumeralExtenso
 [console: NumeralExtenso] Receba um número inteiro e exibição por extensão. 
+
+**Exemplo**:
+
+----Numeral Extenso
+Digite qualquer número: 125
+Número por extenso: cento e vinte e cinco
